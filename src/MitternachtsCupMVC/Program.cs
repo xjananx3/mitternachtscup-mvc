7a2bee83fa24069c30b7536a33ca86e4e691e5e1
Jan <janan9980@gmail.com>
2023-11-08 20:51:03 +0100

    Db Connection SQLite hinzugefügt

--- src/MitternachtsCupMVC/Program.cs
+++ src/MitternachtsCupMVC/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MitternachtsCupMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
