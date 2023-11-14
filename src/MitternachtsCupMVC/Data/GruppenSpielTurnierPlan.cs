89d1cfbd90d434cdffc2dd8523d0482cb3b81904
Jan <janan9980@gmail.com>
2023-11-14 13:13:54 +0100

    (Turnierplan-View)Index View angepasst mit verganenen und kommenden spielen

--- src/MitternachtsCupMVC/Data/GruppenSpielTurnierPlan.cs
+++ src/MitternachtsCupMVC/Data/GruppenSpielTurnierPlan.cs
@@ -4,6 +4,7 @@ public class GruppenSpielTurnierPlan
 {
     public string Platte { get; set; }
     public string SpielName { get; set; }
+    public string? StartZeit { get; set; }
     public string? TeamAName { get; set; }
     public string? TeamBName { get; set; }
     public string? Ergebnis { get; set; }
