416fe7d2a16311709e7255ad28147e85d12fa2c1
Jan <janan9980@gmail.com>
2023-11-15 19:09:13 +0100

    (GruppenAnzeigeRepository)ref: spiele in kommende und vergangene Spiel unterteilen

--- src/MitternachtsCupMVC/Interfaces/IGruppenAnzeigeRepository.cs
+++ src/MitternachtsCupMVC/Interfaces/IGruppenAnzeigeRepository.cs
@@ -6,18 +6,25 @@ namespace MitternachtsCupMVC.Interfaces;
 public interface IGruppenAnzeigeRepository
 {
     Task<IEnumerable<TeamInGruppe>> GetGruppeATeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeASpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeASpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeASpiele();
     Task<IEnumerable<TeamInGruppe>> GetGruppeBTeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeBSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeBSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeBSpiele();
     Task<IEnumerable<TeamInGruppe>> GetGruppeCTeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeCSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeCSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeCSpiele();
     Task<IEnumerable<TeamInGruppe>> GetGruppeDTeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeDSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeDSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeDSpiele();
     Task<IEnumerable<TeamInGruppe>> GetGruppeETeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeESpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeESpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeESpiele();
     Task<IEnumerable<TeamInGruppe>> GetGruppeFTeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeFSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeFSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeFSpiele();
     Task<IEnumerable<TeamInGruppe>> GetGruppeGTeams();
-    Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeGSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeGSpiele();
+    Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeGSpiele();
     
 }
--- src/MitternachtsCupMVC/Repository/GruppenAnzeigeRepository.cs
+++ src/MitternachtsCupMVC/Repository/GruppenAnzeigeRepository.cs
@@ -18,9 +18,14 @@ public class GruppenAnzeigeRepository : IGruppenAnzeigeRepository
         return await GetGruppeTeams("Gruppe A");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeASpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeASpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe A");
+    }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeASpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe A");
     }
 
     public async Task<IEnumerable<TeamInGruppe>> GetGruppeBTeams()
@@ -28,19 +33,30 @@ public class GruppenAnzeigeRepository : IGruppenAnzeigeRepository
         return await GetGruppeTeams("Gruppe B");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeBSpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeBSpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe B");
     }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeBSpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe B");
+    }
+    
 
     public async Task<IEnumerable<TeamInGruppe>> GetGruppeCTeams()
     {
         return await GetGruppeTeams("Gruppe C");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeCSpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeCSpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe C");
+    }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeCSpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe C");
     }
 
     public async Task<IEnumerable<TeamInGruppe>> GetGruppeDTeams()
@@ -48,29 +64,46 @@ public class GruppenAnzeigeRepository : IGruppenAnzeigeRepository
         return await GetGruppeTeams("Gruppe D");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeDSpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeDSpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe D");
     }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeDSpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe D");
+    }
+    
 
     public async Task<IEnumerable<TeamInGruppe>> GetGruppeETeams()
     {
         return await GetGruppeTeams("Gruppe E");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeESpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeESpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe E");
     }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeESpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe E");
+    }
+    
 
     public async Task<IEnumerable<TeamInGruppe>> GetGruppeFTeams()
     {
         return await GetGruppeTeams("Gruppe F");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeFSpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeFSpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe F");
+    }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeFSpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe F");
     }
 
     public async Task<IEnumerable<TeamInGruppe>> GetGruppeGTeams()
@@ -78,10 +111,16 @@ public class GruppenAnzeigeRepository : IGruppenAnzeigeRepository
         return await GetGruppeTeams("Gruppe G");
     }
 
-    public Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeGSpiele()
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetKommendeGruppeGSpiele()
     {
-        throw new NotImplementedException();
+        return await GetGruppeKommendeSpiele("Gruppe G");
     }
+
+    public async Task<IEnumerable<GruppenSpielTurnierPlan>> GetVergangeneGruppeGSpiele()
+    {
+        return await GetGruppeVergangeneSpiele("Gruppe G");
+    }
+    
 
     private async Task<IEnumerable<TeamInGruppe>> GetGruppeTeams(string gruppenName)
     {
@@ -106,5 +145,70 @@ public class GruppenAnzeigeRepository : IGruppenAnzeigeRepository
             .ToList();
 
         return teamsInGruppe;
+    }
+
+    private async Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeKommendeSpiele(string gruppenName)
+    {
+        var ergebnisse = await _context.Ergebnisse.ToListAsync();
+        var teams = await _context.Teams.ToListAsync();
+
+        var spiele = await _context.Spiele
+            .Where(s => s.Name.Contains(gruppenName))
+            .ToListAsync();
+
+        var spieleOhneErgebnis = spiele
+            .Where(s => !ergebnisse.Any(e => e.SpielId == s.Id))
+            .Select(spiel => new GruppenSpielTurnierPlan()
+            {
+                Platte = spiel.Platte.ToString(),
+                SpielId = spiel.Id,
+                SpielName = spiel.Name,
+                StartZeit = spiel.StartZeit.ToString(),
+                TeamAId = teams.FirstOrDefault(t => t.Id == spiel.TeamAId)?.Id,
+                TeamAName = teams.FirstOrDefault(t => t.Id == spiel.TeamAId)?.Name,
+                TeamBId = teams.FirstOrDefault(t => t.Id == spiel.TeamBId)?.Id,
+                TeamBName = teams.FirstOrDefault(t => t.Id == spiel.TeamBId)?.Name
+            }).ToList();
+        
+
+        return spieleOhneErgebnis;
+    }
+    
+    private async Task<IEnumerable<GruppenSpielTurnierPlan>> GetGruppeVergangeneSpiele(string gruppenName)
+    {
+        var ergebnisse = await _context.Ergebnisse.ToListAsync();
+        var teams = await _context.Teams.ToListAsync();
+        
+        var spiele = await _context.Spiele
+            .Where(s => s.Name.Contains(gruppenName))
+            .ToListAsync();
+        
+        var spieleMitErgebnis = spiele
+            .Join(ergebnisse,
+                s => s.Id,
+                e => e.SpielId,
+                (spiel, ergebnis) => new { Spiel = spiel, Ergebnis = ergebnis })
+            .Select(item =>
+            {
+                var teamA = teams.FirstOrDefault(t => t.Id == item.Spiel.TeamAId);
+                var teamB = teams.FirstOrDefault(t => t.Id == item.Spiel.TeamBId);
+
+                return new GruppenSpielTurnierPlan
+                {
+                    Platte = item.Spiel.Platte.ToString(),
+                    SpielName = item.Spiel.Name,
+                    TeamAName = teamA?.Name,
+                    TeamBName = teamB?.Name,
+                    Ergebnis = ErgebnisAufbereiten(item.Ergebnis.PunkteTeamA, item.Ergebnis.PunkteTeamB),
+                    GewinnerName = item.Ergebnis.PunkteTeamA > item.Ergebnis.PunkteTeamB ? teamA?.Name : teamB?.Name
+                };
+            }).ToList();
+
+        return spieleMitErgebnis;
+    }
+    
+    private string ErgebnisAufbereiten(int punkteA, int punkteB)
+    {
+        return $"{punkteA} : {punkteB}";
     }
 }
