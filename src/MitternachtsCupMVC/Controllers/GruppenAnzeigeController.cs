6401beb98c59be1a2992cd22e4adb21d75c958f2
Jan <janan9980@gmail.com>
2023-11-15 19:10:04 +0100

    (Ergebnis-Controller)ref: kommende und Vergangene Spiele in Index bringen

--- src/MitternachtsCupMVC/Controllers/GruppenAnzeigeController.cs
+++ src/MitternachtsCupMVC/Controllers/GruppenAnzeigeController.cs
@@ -15,22 +15,56 @@ public class GruppenAnzeigeController : Controller
     public async Task<IActionResult> Index()
     {
         var teamsA = await _gruppenAnzeigeRepository.GetGruppeATeams();
+        var kommendeSpieleA = await _gruppenAnzeigeRepository.GetKommendeGruppeASpiele();
+        var vergangeneSpieleA = await _gruppenAnzeigeRepository.GetVergangeneGruppeASpiele();
+        
         var teamsB = await _gruppenAnzeigeRepository.GetGruppeBTeams();
+        var kommendeSpieleB = await _gruppenAnzeigeRepository.GetKommendeGruppeBSpiele();
+        var vergangeneSpieleB = await _gruppenAnzeigeRepository.GetVergangeneGruppeBSpiele();
+        
         var teamsC = await _gruppenAnzeigeRepository.GetGruppeCTeams();
+        var kommendeSpieleC = await _gruppenAnzeigeRepository.GetKommendeGruppeCSpiele();
+        var vergangeneSpieleC = await _gruppenAnzeigeRepository.GetVergangeneGruppeCSpiele();
+        
         var teamsD = await _gruppenAnzeigeRepository.GetGruppeDTeams();
+        var kommendeSpieleD = await _gruppenAnzeigeRepository.GetKommendeGruppeDSpiele();
+        var vergangeneSpieleD = await _gruppenAnzeigeRepository.GetVergangeneGruppeDSpiele();
+        
         var teamsE = await _gruppenAnzeigeRepository.GetGruppeETeams();
+        var kommendeSpieleE = await _gruppenAnzeigeRepository.GetKommendeGruppeESpiele();
+        var vergangeneSpieleE = await _gruppenAnzeigeRepository.GetVergangeneGruppeESpiele();
+        
         var teamsF = await _gruppenAnzeigeRepository.GetGruppeFTeams();
+        var kommendeSpieleF = await _gruppenAnzeigeRepository.GetKommendeGruppeFSpiele();
+        var vergangeneSpieleF = await _gruppenAnzeigeRepository.GetVergangeneGruppeFSpiele();
+        
         var teamsG = await _gruppenAnzeigeRepository.GetGruppeGTeams();
+        var kommendeSpieleG = await _gruppenAnzeigeRepository.GetKommendeGruppeGSpiele();
+        var vergangeneSpieleG = await _gruppenAnzeigeRepository.GetVergangeneGruppeGSpiele();
 
         var gruppenAnzeigeVm = new GruppenAnzeigeViewModel()
         {
             GruppeATeams = teamsA,
+            KommendeGruppeASpiele = kommendeSpieleA,
+            VergangeneGruppeASpiele = vergangeneSpieleA,
             GruppeBTeams = teamsB,
+            KommendeGruppeBSpiele = kommendeSpieleB,
+            VergangeneGruppeBSpiele = vergangeneSpieleB,
             GruppeCTeams = teamsC,
+            KommendeGruppeCSpiele = kommendeSpieleC,
+            VergangeneGruppeCSpiele = vergangeneSpieleC,
             GruppeDTeams = teamsD,
+            KommendeGruppeDSpiele = kommendeSpieleD,
+            VergangeneGruppeDSpiele = vergangeneSpieleD,
             GruppeETeams = teamsE,
+            KommendeGruppeESpiele = kommendeSpieleE,
+            VergangeneGruppeESpiele = vergangeneSpieleE,
             GruppeFTeams = teamsF,
-            GruppeGTeams = teamsG
+            KommendeGruppeFSpiele = kommendeSpieleF,
+            VergangeneGruppeFSpiele = vergangeneSpieleF,
+            GruppeGTeams = teamsG,
+            KommendeGruppeGSpiele = kommendeSpieleG,
+            VergangeneGruppeGSpiele = vergangeneSpieleG
         };
         
         return View(gruppenAnzeigeVm);
--- src/MitternachtsCupMVC/ViewModels/GruppenAnzeigeViewModel.cs
+++ src/MitternachtsCupMVC/ViewModels/GruppenAnzeigeViewModel.cs
@@ -6,11 +6,24 @@ namespace MitternachtsCupMVC;
 public class GruppenAnzeigeViewModel
 {
     public IEnumerable<TeamInGruppe> GruppeATeams { get; set; }
-    public IEnumerable<GruppenSpielTurnierPlan>? GruppeASpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeASpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeASpiele { get; set; }
     public IEnumerable<TeamInGruppe> GruppeBTeams { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeBSpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeBSpiele { get; set; }
     public IEnumerable<TeamInGruppe> GruppeCTeams { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeCSpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeCSpiele { get; set; }
     public IEnumerable<TeamInGruppe> GruppeDTeams { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeDSpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeDSpiele { get; set; }
     public IEnumerable<TeamInGruppe> GruppeETeams { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeESpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeESpiele { get; set; }
     public IEnumerable<TeamInGruppe> GruppeFTeams { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeFSpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeFSpiele { get; set; }
     public IEnumerable<TeamInGruppe> GruppeGTeams { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> VergangeneGruppeGSpiele { get; set; }
+    public IEnumerable<GruppenSpielTurnierPlan> KommendeGruppeGSpiele { get; set; }
 }
