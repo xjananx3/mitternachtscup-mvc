03a0a27eae37fb3628a3a06acfbce2efc398f9a5
Jan <janan9980@gmail.com>
2023-11-17 17:57:28 +0100

    (KoPhase-View)Finale und SpielumPlatz3 Views erstellt mit FinalSpielViewModel.cs

---
+++ src/MitternachtsCupMVC/ViewModels/FinalSpielViewModel.cs
@@ -0,0 +1,15 @@
+namespace MitternachtsCupMVC;
+
+public class FinalSpielViewModel
+{
+    public string Platte { get; set; }
+    public int SpielId { get; set; }
+    public string SpielName { get; set; }
+    public string? StartZeit { get; set; }
+    public int? TeamAId { get; set; }
+    public string? TeamAName { get; set; }
+    public int? TeamBId { get; set; }
+    public string? TeamBName { get; set; }
+    public string? Ergebnis { get; set; }
+    public string? GewinnerName { get; set; }
+}
