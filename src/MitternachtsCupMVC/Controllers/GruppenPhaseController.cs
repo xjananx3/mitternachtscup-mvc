946ef59e8ac09213f22eadebb55b16165ab42b59
Jan <janan9980@gmail.com>
2023-11-17 17:59:25 +0100

    (KoPhase-View)Unterreiter für Achtelfinale, Viertelfinale, Halbfinale, SpielUmPlatz3 und Finale erstellen

--- src/MitternachtsCupMVC/Controllers/GruppenPhaseController.cs
+++ src/MitternachtsCupMVC/Controllers/GruppenPhaseController.cs
@@ -69,4 +69,110 @@ public class GruppenPhaseController : Controller
         
         return View(gruppenAnzeigeVm);
     }
+
+    public async Task<IActionResult> GruppeA()
+    {
+        var teamsA = await _gruppenPhaseRepository.GetGruppeATeams();
+        var kommendeSpieleA = await _gruppenPhaseRepository.GetKommendeGruppeASpiele();
+        var vergangeneSpieleA = await _gruppenPhaseRepository.GetVergangeneGruppeASpiele();
+
+        var gruppenAnzeigeVm = new GruppenPhaseViewModel
+        {
+            GruppeATeams = teamsA,
+            KommendeGruppeASpiele = kommendeSpieleA,
+            VergangeneGruppeASpiele = vergangeneSpieleA
+        };
+
+        return View(gruppenAnzeigeVm);
+    }
+    
+    public async Task<IActionResult> GruppeB()
+    {
+        var teamsB = await _gruppenPhaseRepository.GetGruppeBTeams();
+        var kommendeSpieleB = await _gruppenPhaseRepository.GetKommendeGruppeBSpiele();
+        var vergangeneSpieleB = await _gruppenPhaseRepository.GetVergangeneGruppeBSpiele();
+
+        var gruppenPhaseVm = new GruppenPhaseViewModel
+        {
+            GruppeBTeams = teamsB,
+            KommendeGruppeBSpiele = kommendeSpieleB,
+            VergangeneGruppeBSpiele = vergangeneSpieleB
+        };
+        return View(gruppenPhaseVm);
+    }
+    
+    public async Task<IActionResult> GruppeC()
+    {
+        var teamsC = await _gruppenPhaseRepository.GetGruppeCTeams();
+        var kommendeSpieleC = await _gruppenPhaseRepository.GetKommendeGruppeCSpiele();
+        var vergangeneSpieleC = await _gruppenPhaseRepository.GetVergangeneGruppeCSpiele();
+
+        var gruppenPhaseVm = new GruppenPhaseViewModel
+        {
+            GruppeCTeams = teamsC,
+            KommendeGruppeCSpiele = kommendeSpieleC,
+            VergangeneGruppeCSpiele = vergangeneSpieleC
+        };
+        return View(gruppenPhaseVm);
+    }
+    
+    public async Task<IActionResult> GruppeD()
+    {
+        var teamsD = await _gruppenPhaseRepository.GetGruppeDTeams();
+        var kommendeSpieleD = await _gruppenPhaseRepository.GetKommendeGruppeDSpiele();
+        var vergangeneSpieleD = await _gruppenPhaseRepository.GetVergangeneGruppeDSpiele();
+
+        var gruppenPhaseVm = new GruppenPhaseViewModel
+        {
+            GruppeDTeams = teamsD,
+            KommendeGruppeDSpiele = kommendeSpieleD,
+            VergangeneGruppeDSpiele = vergangeneSpieleD
+        };
+        return View(gruppenPhaseVm);
+    }
+    
+    public async Task<IActionResult> GruppeE()
+    {
+        var teamsE = await _gruppenPhaseRepository.GetGruppeETeams();
+        var kommendeSpieleE = await _gruppenPhaseRepository.GetKommendeGruppeESpiele();
+        var vergangeneSpieleE = await _gruppenPhaseRepository.GetVergangeneGruppeESpiele();
+
+        var gruppenPhaseVm = new GruppenPhaseViewModel
+        {
+            GruppeETeams = teamsE,
+            KommendeGruppeESpiele = kommendeSpieleE,
+            VergangeneGruppeESpiele = vergangeneSpieleE
+        };
+        return View(gruppenPhaseVm);
+    }
+    
+    public async Task<IActionResult> GruppeF()
+    {
+        var teamsF = await _gruppenPhaseRepository.GetGruppeFTeams();
+        var kommendeSpieleF = await _gruppenPhaseRepository.GetKommendeGruppeFSpiele();
+        var vergangeneSpieleF = await _gruppenPhaseRepository.GetVergangeneGruppeFSpiele();
+
+        var gruppenPhaseVm = new GruppenPhaseViewModel
+        {
+            GruppeFTeams = teamsF,
+            KommendeGruppeFSpiele = kommendeSpieleF,
+            VergangeneGruppeFSpiele = vergangeneSpieleF
+        };
+        return View(gruppenPhaseVm);
+    }
+    
+    public async Task<IActionResult> GruppeG()
+    {
+        var teamsG = await _gruppenPhaseRepository.GetGruppeGTeams();
+        var kommendeSpieleG = await _gruppenPhaseRepository.GetKommendeGruppeGSpiele();
+        var vergangeneSpieleG = await _gruppenPhaseRepository.GetVergangeneGruppeGSpiele();
+
+        var gruppenPhaseVm = new GruppenPhaseViewModel
+        {
+            GruppeGTeams = teamsG,
+            KommendeGruppeGSpiele = kommendeSpieleG,
+            VergangeneGruppeGSpiele = vergangeneSpieleG
+        };
+        return View(gruppenPhaseVm);
+    }
 }
