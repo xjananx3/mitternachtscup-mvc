d0ad183a5c14fbb15e15e80431a5d609ac0e4f04
Jan <janan9980@gmail.com>
2023-11-17 22:11:42 +0100

    (Ergebnis-View)Edit mit zugehörigem EditErgebnisViewModel hinzugefügt

---
+++ src/MitternachtsCupMVC/ViewModels/EditErgebnisViewModel.cs
@@ -0,0 +1,14 @@
+using MitternachtsCupMVC.Models;
+
+namespace MitternachtsCupMVC;
+
+public class EditErgebnisViewModel
+{
+    public int Id { get; set; }
+    public int PunkteTeamA { get; set; }
+    public int PunkteTeamB { get; set; }
+    public int SpielId { get; set; }
+    public Spiel Spiel { get; set; }
+    public int TeamId { get; set; }
+    public Team GewinnerTeam { get; set; }
+}
