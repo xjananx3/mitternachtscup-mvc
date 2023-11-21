ff33ac9ef3054e33472058f9faa0655f35522764
Jan <janan9980@gmail.com>
2023-11-21 13:59:33 +0100

    (GruppenPhase-View) Alle Gruppen mit Vm erstellt

---
+++ src/MitternachtsCupMVC/ViewModels/AlleGruppenViewModel.cs
@@ -0,0 +1,16 @@
+using MitternachtsCupMVC.Models;
+
+namespace MitternachtsCupMVC;
+
+public class AlleGruppenViewModel
+{
+    public IEnumerable<Team> GruppeA { get; set; }
+    public IEnumerable<Team> GruppeB { get; set; }
+    public IEnumerable<Team> GruppeC { get; set; }
+    public IEnumerable<Team> GruppeD { get; set; }
+    public IEnumerable<Team> GruppeE { get; set; }
+    public IEnumerable<Team> GruppeF { get; set; }
+    public IEnumerable<Team> GruppeG { get; set; }
+    
+    
+}
